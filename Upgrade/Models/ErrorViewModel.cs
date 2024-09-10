62e08fb9294892cfc9dd59deb827c7f7119270ff
Jim <wsxedc0728@gmail.com>
2024-09-10 10:24:15 +0800

    加入專案檔案。

---
+++ Upgrade/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Upgrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
